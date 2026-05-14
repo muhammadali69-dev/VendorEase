e430925330331a3936c8e8e8b4a0290e9b9f50ff
muhammadali69-dev <muhammadalitech05@gmail.com>
2026-05-14 17:26:24 +0530

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from supabase_client import supabase
+
+
+def login():
+
+    st.subheader("🔐 Vendor Login")
+
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+
+        try:
+            response = supabase.auth.sign_in_with_password({
+                "email": email,
+                "password": password
+            })
+
+            st.session_state["user"] = response.user
+            st.success("Login Successful!")
+            st.rerun()
+
+        except Exception as e:
+            st.error("Invalid Email or Password")
+
+
+def signup():
+
+    st.subheader("📝 Create Account")
+
+    email = st.text_input("New Email")
+    password = st.text_input("New Password", type="password")
+
+    if st.button("Sign Up"):
+
+        try:
+            supabase.auth.sign_up({
+                "email": email,
+                "password": password
+            })
+
+            st.success("Account Created! Please Login.")
+
+        except Exception as e:
+            st.error("Signup Failed")
+
+
+def logout():
+
+    if st.sidebar.button("Logout"):
+
+        supabase.auth.sign_out()
+
+        st.session_state.clear()
+
+        st.success("Logged Out")
+
+        st.rerun()
